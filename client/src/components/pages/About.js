56b457e1c24c69e2c545b1409398e9122f30190e
c-masque <maudlin.oxalis@gmail.com>
2021-06-25 10:28:05 -0600

    Created about page, added some basic styles and content.

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import me from "../../images/me.png"
+
 function About() {
     return (
         <div className="about">
-            <h1>About Me</h1>
+            <div className="container">
+                <div className="picofme">
+                    <img src={me} alt="picture of maud"/>
+                </div>
+                <div className="content-wrapper">
+                    <div className="about-description">
+                        Maudlin is an artist from Missoula MT. There is not one particular theme throughout all work, but it definitely involves the occasional tentacles mixing into waves and body horror.<br/><br/><br/><br/>
+                        ▂▃▅▇█▓▒░ Also she coded this entire website ░▒▓█▇▅▃▂
+                    </div> 
+                    <div className="featured-works">
+                        <h2>Where to find me and my work</h2>
+                        <ul>
+                            <li>Painted Clocks, located at the Loose Moose in downtown Missoula Montana </li>
+                            <li>Self-hosted small public art night events during the fall</li>
+                            <li>Portrait drawing in downtown Coeur d'Alene Idaho semi-frequently in the summer</li>
+                            <li>Slant Street Zine, issues 000-004</li>
+                            <li>Instagram</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
