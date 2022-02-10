2c4ea4876ed7546181bf6e072a8ad8f1f97572ae
c-masque <maudlin.oxalis@gmail.com>
2022-02-10 01:58:18 -0700

    Cleaned up some code.

--- client/src/components/pages/Gallery/GalleryContainer.js
+++ client/src/components/pages/Gallery/GalleryContainer.js
@@ -34,36 +34,6 @@ export default function GalleryContainer() {
             })
     }
 
-
-    // function fetchGalleryItems() {
-    //     axios
-    //         .get(`${slinky}artwork/page/${page}`)
-    //         .then(res => {
-    //             console.log('this is the response:', res, typeof res)
-    //             setGalleryData(
-    //                 {...res}
-    //             )
-    //         })
-
-    //         .catch(error => {
-    //             console.log("There was an error retrieving the gallery items...", error);
-    //           })
-    // }
-
-    // getGalleryItems = () => {
-    //     axios
-    //         .get(`${slinky}artwork/page/${this.state.page}`)
-    //         .then(res => {
-    //             this.setState({
-    //                 galleryData: res.data
-    //             })
-    //         })
-
-    //         .catch(error => {
-    //             console.log("There was an error retrieving the gallery items", error);
-    //           })
-    // }
-
     function nextPage() {
 
         page.current = page.current + 1
@@ -106,61 +76,11 @@ export default function GalleryContainer() {
         )
     }
 
-    // galleryItems() {
-    //     return (
-
-    //         <InfiniteScroll
-    //             dataLength={this.state.galleryData.length}
-    //             next={() => this.setPage(page + 1)}
-    //         >
-
-    //             {this.state.galleryData.map(artwork => {
-
-    //             const {
-    //                 _id
-    //             } = artwork
-
-    //             return (
-    //                 <Link key={_id} to={{
-    //                     pathname: `/g/${_id}`,
-    //                     artwork: artwork
-    //                 }}>
-    //                     <div className="gallery-item">
-    //                         <img src={artwork.url} alt={`${artwork.title} painting`}/>
-    //                     </div>
-    //                 </Link>
-    //             )
-    //             })}
-
-    //         </InfiniteScroll>
-    //     )
-        
-    // }
-
-    // componentDidMount() {
-    //     this.setState({
-    //         loading: true
-    //     })
-    //     this.getGalleryItems()
-    //     this.setState({
-    //         loading: false
-    //     })
-    // }
-
     return (
         <>
-            {/* {
-                this.state.loading ? 
-                <ClipLoader
-                    size={50}
-                    color={'#ffffff'}
-                    loading={this.state.loading}
-                />
-                : */}
                 <div className="gallery-grid">
                     {galleryItems()}
                 </div>
-            {/* } */}
         </>
     )
 }
